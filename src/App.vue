9a36ff5dc2b90cc63713f2a501076ec072013047
lparamedicl <kondaurovr1@gmail.com>
2023-07-28 17:28:46 +0500

    pzdcя забыл как этим пользоваться всем.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+  <RouterView />
+</template>
+
---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-black">
+    <h1 class="bg-red-600">This is an about page</h1>
+  </div>
+</template>
