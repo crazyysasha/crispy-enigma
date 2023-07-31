898de6edcd07cf3aee5cecf559771143814eae66
Otash1727 <128920121+Otash1727@users.noreply.github.com>
2023-07-31 07:44:45 +0300

    new fixx

---
+++ crispy-enigma/src/components/SecondLine.vue
@@ -0,0 +1,56 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container flex flex-col p-10 text-xl">
+        <div class="flex gap-32">
+            <div class="font-PlusJakartaSans  flex flex-col gap-4">
+                <h1 class="text-3xl font-bold">Hi, Skylar Dias</h1>
+                <p>Let's finish your task today!</p>
+            </div>  
+            <div class="flex gap-3 p-6">
+                <div class="p-4">
+                    <button>
+                        <img src="src/assets/images/SecondLinePictures/Notification.png" alt="">
+                    </button>    
+                </div>
+                <div>
+                    <img src="src/assets/images/SecondLinePictures/Profil.png" alt="" class="w-16">
+                </div>
+            </div>
+        </div>
+        <div class="flex">
+            <div class="bg-[#141522] text-white rounded-lg flex flex-col items-center w-64 h-52 gap-3 justify-center">
+                <div class="flex flex-col gap-3 mr-16"> 
+                    <p>Running Task</p>
+                    <p class="text-3xl font-bold">65</p>
+                </div>
+                <div class="flex gap-6 mr-8">
+                    <p class="text-2xl font-bold">45%</p>
+                    <div class='flex flex-col gap-3'>
+                        <p class="text-3xl font-bold">100</p>
+                        <p>Tasks</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col gap-6">
+                <p>Activity</p>
+                <div class=" flex outline-offset-8 outline outline-white">
+                    
+                    <div>
+                        <ul>
+                            <li>3</li>
+                            <li>2</li>
+                            <li>1</li>
+                            
+                        </ul>
+                    </div>
+                        <img src="src/assets/images/SecondLinePictures/diagramma.svg" alt="">
+                    </div>
+                    <div class="bg-[#141522] rounded-lg text-white w-16 h-12">
+                        <p>2Tasks</p>
+                    </div>
+            </div>          
+        </div>
+    </div>
+</template>
--- crispy-enigma/src/views/HomeView.vue
+++ crispy-enigma/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import FirstLine from '../components/FirstLine.vue';
+import SecondLine from '../components/SecondLine.vue';
 </script>
 
 <template>
-    <div class=" bg-teal-800 min-h-screen">
+    <div class=" bg-teal-800 min-h-screen flex">
       <FirstLine></FirstLine>
+      <SecondLine></SecondLine>
     </div>  
 </template>
