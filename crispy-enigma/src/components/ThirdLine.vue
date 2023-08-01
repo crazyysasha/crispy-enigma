b5e38a4242e3dcadd2d2db8794b9527819cd3148
Otash1727 <128920121+Otash1727@users.noreply.github.com>
2023-08-01 17:42:41 +0300

    new fix

--- crispy-enigma/src/components/ThirdLine.vue
+++ crispy-enigma/src/components/ThirdLine.vue
@@ -1,34 +1,46 @@
 <script setup>
+import ThirdLineRow from './minicomponents/ThirdLineRow.vue';
 </script>
 
 <template>
-    <div class="container flex flex-col gap-5 font-PlusJakartaSans text-xl p-10 ">
+    <div class="container flex flex-col gap-5 font-PlusJakartaSans text-xl p-10 w-96">
         <div class="flex justify-between">
             <button>&#10094;</button>
             <p class="font-bold">July 2022</p>
             <button>&#x276F;</button>
         </div>
         <div>
-            <ul class="inline-flex gap-8">
-                <li>S</li>
-                <li>M</li>
-                <li>T</li>
-                <li>W</li>
-                <li>T</li>
-                <li>F</li>
-                <li>S</li>
-            </ul>
+            <div class="flex gap-5">
+                <ul>
+                    <li>S</li>
+                    <li>10</li>
+                </ul>
+                <ul>
+                    <li>M</li>
+                    <li>11</li>
+                </ul>
+                <ul>
+                    <li>T</li>
+                    <li>12</li>
+                </ul>
+                <ul class="bg-black rounded-xl text-white w-6">
+                    <li>W</li>
+                    <li class="rounded-full bg-blue-600">13</li>
+                </ul>
+                <ul>
+                    <li>T</li>
+                    <li>14</li>
+                </ul>
+                <ul>
+                    <li>F</li>
+                    <li>15</li>
+                </ul>
+                <ul>
+                    <li>S</li>
+                    <li>16</li>
+                </ul>
             </div>
-            <div>
-            <ul class="inline-flex gap-7">
-                <li>10</li>
-                <li>11</li>
-                <li>12</li>
-                <li>13</li>
-                <li>14</li>
-                <li>15</li>
-                <li>16</li>
-            </ul>
         </div>
+        <ThirdLineRow></ThirdLineRow>
     </div>
 </template>
---
+++ crispy-enigma/src/components/minicomponents/ThirdLineRow.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <div class="flex font-bold text-xl justify-between">
+            <h1>Task Today</h1>
+            <button>...</button>
+        </div>
+
+    </div>
+</template>
