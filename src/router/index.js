23398e7331fcf78d494d389b4b0406ab9bb07a6e
kozziewozzie <kozzie_wozzie@icloud.com>
2023-08-04 18:55:23 +0500

    delete transition in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import HomeView from '../views/HomeView.vue'
 
 import DefaultLayout from '../layouts/DefaultLayout.vue';
 import AuthLayout from '../layouts/AuthLayout.vue'
-import { Transition } from 'vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
