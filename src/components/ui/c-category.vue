c32b675d289395def9b4f0f17d4b845c42904102
kozziewozzie <kozzie_wozzie@icloud.com>
2023-08-06 00:54:29 +0500

    -path changed for icons -added components 'c-circle', 'c-category' -header added

---
+++ src/components/ui/c-category.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+    label: {},
+    img: {}
+})
+</script>
+
+
+<template>
+    <button class="px-7 py-3.5 border border-[#F5F5F7] rounded-xl flex w-max h-14">
+        <div>
+            <img :src="img" alt="">
+        </div>
+        <div class="pl-3 text-center">
+        <slot>
+            {{  label }}
+        </slot>
+        </div>
+    </button>
+</template>
+
+<!-- 
+<template>
+    <div class="px-7 py-3 border border-[#F5F5F7] rounded-xl items-start flex w-max">
+        <div>
+        <img src="/src/components/icons/element_1.svg" alt="">
+    </div>
+    <div class="pl-3">
+        Category
+    </div>
+    </div>
+</template> -->
---
+++ src/views/examples/HeaderViewEx.vue
@@ -0,0 +1,6 @@
+<script setup>
+import HeaderMain from '/src/components/ui/HeaderMain'
+</script>
+<template>
+<HeaderMain />
+</template>
