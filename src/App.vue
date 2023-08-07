4d36ccde4abc2fe8cac26e954d91025423ba181b
lparamedicl <kondaurovr1@gmail.com>
2023-08-07 17:50:04 +0500

    chilling

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  
+        <RouterLink to="/home">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/nav">Nav</RouterLink>
+  <RouterView />
 </template>
 
