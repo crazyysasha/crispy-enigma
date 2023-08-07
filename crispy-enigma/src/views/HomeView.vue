49e40531d8836713d0743ffeaedbaecb58065a0a
Sasha Raimov <crazyysasha@gmail.com>
2023-08-07 18:45:09 +0500

    Revert "bug fixes"
    This reverts commit 1f3872afb881fd6619dfd20bac659637fdc272da.

--- crispy-enigma/src/views/HomeView.vue
+++ crispy-enigma/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
+import FirstLine from '../components/FirstLine.vue';
 import SecondLine from '../components/SecondLine.vue';
 import ThirdLine from '../components/ThirdLine.vue';
 </script>
 
 <template>
-  <SecondLine></SecondLine>
-  <ThirdLine></ThirdLine>
+    <div class=" bg-teal-800 min-h-screen flex">
+      <FirstLine></FirstLine>
+      <SecondLine></SecondLine>
+      <ThirdLine></ThirdLine>
+    </div>  
 </template>
