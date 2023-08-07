cfcbc881350461c55bc3709fbc246d21c0141346
lparamedicl <kondaurovr1@gmail.com>
2023-08-07 14:42:52 +0500

    made running task card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,27 @@
 </script>
 
 <template>
-
+<div class="flex">
+  <div class="flex"><div class="w-[200px] h-[215px] bg-black text-white rounded-2xl px-[30px] flex flex-col justify-evenly">
+    <div class="font-semibold text-base/8h ">Running Task</div>
+    <div class="font-semibold text-4xl/[48px]">65</div>
+    <div class="flex justify-around items-center"><div class="rounded-full py-[22px] px-[19px] font-semibold text-lg/10h border border-[#546FFF] border-4">45%</div> <div class="flex flex-col">100 <br> <div class="text-[#8E92BC]">Task</div></div>  </div>
+  </div><div class="flex bg-stone-400"></div></div>
+  <div></div>
+  <div></div>
+</div>
 </template>
+
+
+<style>
+.plusjakarta {
+  font-family: Plus Jakarta Sans;
+font-size: 16px;
+font-style: normal;
+font-weight: 600;
+line-height: 24px; 
+letter-spacing: -0.32px;
+}
+
+
+</style>
