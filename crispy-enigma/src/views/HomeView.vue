e0301130c2cd74dc6ecd797e1bcbb784e47529a8
Sasha Raimov <crazyysasha@gmail.com>
2023-08-07 18:49:43 +0500

    Revert "reperied"
    This reverts commit e308f27692269872f170803d88db679629507e14.

--- crispy-enigma/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import FirstLine from '../components/FirstLine.vue';
-</script>
-
-<template>
-    <div class=" bg-teal-800 min-h-screen">
-      <FirstLine></FirstLine>
-    </div>  
-</template>
